b941a995d8e215a67e29103ead5d2f8519a63494
FranSarrio <64607833+FranSarrio@users.noreply.github.com>
2020-05-18 18:23:26 -0300

    Create test.js

---
+++ TPESTUDIO/test.js
@@ -0,0 +1,140 @@
+VALOR_HORA= 100
+
+Clientes = [ 
+
+{
+	Nombre: 'La Renga',
+	Telefono: 2323232124,
+	Sesiones: [ 
+		{
+			duracion: 2, 
+			fecha: '23/04/2019'
+		}, 
+		{
+			duracion: 2.5, 
+			fecha: '29/04/2019'
+		}
+  	],
+	Pagos: [ 
+		{
+			fecha: '20/04/2019',
+			monto: 140
+		},
+		{	
+			fecha: '20/05/2019',
+			monto: 89
+
+		}
+	]
+	
+},
+
+{
+
+	Nombre: 'NAFTA',
+	Telefono: 23232321234,
+	Sesiones: [
+		{
+			duracion: 1,
+			fecha: '23/04/2019'
+		}, 
+		{
+			duracion: 2, 
+			fecha: '29/04/2019'
+		}
+		],
+	Pagos: [
+		{
+			fecha: '23/04/2019',
+			monto: 290
+		},
+		{
+			fecha: '25/05/2019',
+			monto: 45
+		}
+	]
+	
+}, 
+
+{
+	Nombre: 'La Maquina de hacer pajaros',
+	Telefono: 2323232124,
+	Sesiones: [ 
+		{
+			duracion: 4, 
+			fecha: '23/04/2019'
+		}, 
+
+		{
+			duracion: 3, 
+			fecha: '29/04/2019'
+		}
+	],
+	Pagos: [
+
+		{
+			fecha: '22/04/2019',
+			monto: 290
+		},
+		{
+			fecha: '25/05/2019',
+			monto: 100
+		}
+	]
+
+},
+
+{
+	Nombre: 'GAS OIL',
+	Telefono: 2323232124,
+	Sesiones: [ 
+		{
+			duracion: 4, 
+			fecha: '23/04/2019'
+		}, 
+
+		{
+			duracion: 9, 
+			fecha: '29/04/2019'
+		}
+	],
+
+	Pagos: [	
+		{
+			fecha: '18/04/2019',
+			monto: 800
+		},
+		{
+			fecha: '2/05/2019',
+			monto: 200
+		}
+	]
+},
+
+{
+	Nombre: 'La fabrica de golondrinas',
+	Telefono: 2323232124,
+	Sesiones: [ 
+		{
+			duracion: 12, 
+			fecha: '23/04/2019'
+		}, 
+		{
+			duracion: 18, 
+			fecha: '29/04/2019'
+		}
+	],
+	Pagos: [
+		{
+			fecha: '19/04/2019',
+			monto: 1000
+		},
+		{
+			fecha: '03/05/2019',
+			monto: 1500
+		}
+	]
+
+}
+
+]
