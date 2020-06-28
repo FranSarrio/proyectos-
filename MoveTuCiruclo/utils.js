81d20ba76731c5da61f5dc62357190d519f68400
FranSarrio <64607833+FranSarrio@users.noreply.github.com>
2020-06-28 15:59:58 -0300

    Update utils.js

--- MoveTuCiruclo/utils.js
+++ MoveTuCiruclo/utils.js
@@ -1,13 +1,3 @@
-  	const flechaIzquierda= document.querySelector('.left-row')
-	const flechaDerecha= document.querySelector('.right-row')
-	const flechaArriba= document.querySelector('.up-row')
-	const flechaAbajo= document.querySelector('.bottom-row')
-	const randomize= document.querySelector('.randomize')
-	const diagonal= document.querySelector('.diagonal')
-	const colorRojo= document.querySelector('.red')
-	const colorVerde= document.querySelector('.green')
-	const colorAzul= document.querySelector('.blue')
-	const circuloPrincipal= document.querySelector('.principal-circle')
 
 	function moveLeft () {
   		let principalCircleStyle= window.getComputedStyle(circuloPrincipal)
@@ -54,3 +44,5 @@
 			circuloPrincipal.style.left= '-188px'
 		}
  	}
+
+  	
